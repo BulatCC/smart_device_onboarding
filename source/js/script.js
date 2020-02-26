e9f5df64f6a5854fdcf27503e126dc2f0e42ebcd
BulatCC <4134236@gmail.com>
2020-02-26 09:52:15 +0500

    доделал JS, сделал пиксель перфект, исправил ошибки npm test

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,112 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+(function () {
+  // тэг body
+  var bodyTag = document.querySelector('body');
 
-pageHeader.classList.remove('page-header--nojs');
+  // кнопка 'заказать звонок'
+  var callButton = document.querySelector('.page-header__contacts-button');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+  // модальное окно
+  var modal = document.querySelector('.modal');
+
+  // кнопка закрытия модального окна
+  var modalCloseButton = document.querySelector('.modal-button');
+
+  // заголовок "разделы сайта"
+  var siteMapTitle = document.querySelector('.nav__title');
+
+  // список разделов сайта
+  var siteMapList = document.querySelector('.nav__list');
+
+  // заголовок раздела "Контакты"
+  var contactsTitle = document.querySelector('.contacts__title');
+
+  // список разделов сайта
+  var contactsList = document.querySelector('.contacts__list');
+
+  // поле имя
+  var userName = modal.querySelector('#name-callback');
+
+  // поле номер телефона
+  var phoneNumber = modal.querySelector('#phone-callback');
+
+  // текстовое поле
+  var textField = modal.querySelector('#ask-question-callback');
+
+  // форма
+  var form = modal.querySelector('.modal__form');
+
+  // флаг для проверки работы localstorage
+  var isLocalStorageEnabled = true;
+
+  // проверка на работоспособность localstorage
+  try {
+    localStorage.getItem('name');
+  } catch (err) {
+    isLocalStorageEnabled = false;
   }
-});
+
+  // сохраняет данные в localstorage
+  var saveData = function () {
+    if (isLocalStorageEnabled) {
+      form.addEventListener('submit', function () {
+        localStorage.setItem('name', userName.value);
+        localStorage.setItem('phone', phoneNumber.value);
+        localStorage.setItem('message', textField.value);
+      });
+    }
+  };
+
+  // аккордеон
+  siteMapTitle.addEventListener('click', function () {
+    siteMapTitle.classList.toggle('accordeon__toggle--active');
+    siteMapList.classList.toggle('accordeon--active');
+  });
+
+  contactsTitle.addEventListener('click', function () {
+    contactsTitle.classList.toggle('accordeon__toggle--active');
+    contactsList.classList.toggle('accordeon--active');
+  });
+
+  // закрывает попап
+  var removeModal = function () {
+    modal.classList.remove('modal--active');
+    bodyTag.classList.remove('disable-scroll');
+  };
+
+  // закрывает попап
+  var closeModal = function () {
+    modalCloseButton.addEventListener('click', function () {
+      removeModal();
+    });
+
+    modal.addEventListener('click', function (evt) {
+      if (evt.target === modal) {
+        removeModal();
+      }
+    });
+
+    document.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        removeModal();
+      }
+    });
+  };
+
+  // валидация по номеру телефона
+  var validation = function () {
+    window.iMask(phoneNumber, {mask: '+{7}(000)000-00-00'});
+  };
+
+  // запускает работу с модальным окном
+  callButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.add('modal--active');
+    bodyTag.classList.add('disable-scroll');
+    userName.focus();
+    closeModal();
+    saveData();
+    validation();
+  });
+
+})();
